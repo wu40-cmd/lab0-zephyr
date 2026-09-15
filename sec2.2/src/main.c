01ec0712054cb805a4fe58f8e08478095500baa7
wu40-cmd <wu40@seas.upenn.edu>
2026-09-15 16:32:13 -0400

    Complete Section 5.1 devicetree LED alias

--- sec2.2/src/main.c
+++ sec2.2/src/main.c
@@ -12,13 +12,13 @@
 #define SLEEP_TIME_MS   2000
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+#define LED5180_NODE DT_ALIAS(led5180)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED5180_NODE, gpios);
 
 int main(void)
 {
